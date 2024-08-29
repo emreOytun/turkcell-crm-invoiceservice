540d8e4861fcf0e072af711c66a451e71961b9f9
emreOytun <emreoytun58@gmail.com>
2024-08-29 22:22:30 +0300

    mockito extend added to test class
    (cherry picked from commit e8a34b00b5f8e996ef6c707e4072a0ca7af423f9)

--- src/test/java/com/turkcell/pair3/invoiceservice/InvoiceserviceApplicationTests.java
+++ src/test/java/com/turkcell/pair3/invoiceservice/InvoiceserviceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.turkcell.pair3.invoiceservice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = InvoiceserviceApplication.class)
+@ExtendWith(MockitoExtension.class)
 class InvoiceserviceApplicationTests {
 
     @Test
