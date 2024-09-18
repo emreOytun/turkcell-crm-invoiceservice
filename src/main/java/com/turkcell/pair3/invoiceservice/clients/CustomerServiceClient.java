00e9df1b0fae3b1a5e497a3bb1264032d02832ba
emreOytun <emreoytun58@gmail.com>
2024-09-18 20:01:17 +0300

    retry mechanism added to feign clients

--- src/main/java/com/turkcell/pair3/invoiceservice/clients/CustomerServiceClient.java
+++ src/main/java/com/turkcell/pair3/invoiceservice/clients/CustomerServiceClient.java
@@ -1,11 +1,13 @@
 package com.turkcell.pair3.invoiceservice.clients;
 
 import com.turkcell.pair3.core.configuration.feign.FeignClientConfiguration;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @FeignClient(name="customerservice", configuration = FeignClientConfiguration.class)
+@Retry(name = "feignclient")
 public interface CustomerServiceClient {
 
     @GetMapping("/api/customers/getInfo")
--- src/main/java/com/turkcell/pair3/invoiceservice/clients/ProductServiceClient.java
+++ src/main/java/com/turkcell/pair3/invoiceservice/clients/ProductServiceClient.java
@@ -1,11 +1,13 @@
 package com.turkcell.pair3.invoiceservice.clients;
 
 import com.turkcell.pair3.core.configuration.feign.FeignClientConfiguration;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "productservice", configuration = FeignClientConfiguration.class)
+@Retry(name = "feignclient")
 public interface ProductServiceClient {
     @GetMapping("/api/products/hasProduct")
     boolean hasAccountProduct(@RequestParam("billAccountId") int accountId);
